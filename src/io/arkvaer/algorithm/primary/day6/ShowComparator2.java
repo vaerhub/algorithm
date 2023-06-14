ec68d13d65b5c1971fefbd8a647a0d531c3fe78e
vaer <arkvaer@gmail.com>
2023-06-14 22:53:10 +0800

    test gpg

--- src/io/arkvaer/algorithm/primary/day6/ShowComparator2.java
+++ src/io/arkvaer/algorithm/primary/day6/ShowComparator2.java
@@ -12,13 +12,7 @@ public class ShowComparator2 {
         // 0, 谁放前都行
         @Override
         public int compare(Integer o1, Integer o2) {
-            if (o1 < o2) {
-                return 1;
-            } else if (o1 > o2) {
-                return -1;
-            } else {
-                return 0;
-            }
+            return o2.compareTo(o1);
         }
 
     }
