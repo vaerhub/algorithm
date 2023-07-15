2aae0977f5f3a97ddb7f8675fddfb06b509f3c79
arkvaer <arkvaer@gmail.com>
2023-07-15 20:04:55 +0800

    add: 完成暴力递归方式验证二叉树是否为完全二叉树和是否为搜索二叉树

---
+++ src/io/arkvaer/algorithm/basic/day12/IsBST.java
@@ -0,0 +1,149 @@
+package io.arkvaer.algorithm.basic.day12;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 判断给定二叉树是否为[搜索二叉树]
+ *
+ * @author waver
+ * @date 2023/7/15 16:45
+ */
+public class IsBST {
+    public static class Node {
+        public int value;
+        public Node left;
+        public Node right;
+
+        public Node(int data) {
+            this.value = data;
+        }
+    }
+
+    /**
+     * 使用中序遍历方式校验是否为搜索二叉树
+     *
+     * @param head 给定的二叉树头节点
+     * @return 是否为搜索二叉树
+     */
+    public static boolean isBST1(Node head) {
+        if (head == null) {
+            return true;
+        }
+        List<Node> nodeList = new ArrayList<>();
+        inOrder(head, nodeList);
+        for (int i = 1; i < nodeList.size(); i++) {
+            if (nodeList.get(i).value <= nodeList.get(i - 1).value) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void inOrder(Node head, List<Node> nodeList) {
+        if (head == null) {
+            return;
+        }
+        inOrder(head.left, nodeList);
+        nodeList.add(head);
+        inOrder(head.right, nodeList);
+    }
+
+
+    public static class Info {
+        /**
+         * 是否为搜索二叉树
+         */
+        boolean isSearch;
+        /**
+         * 最小值
+         */
+        int min;
+        /**
+         * 最大值
+         */
+        int max;
+
+        public Info(boolean isSearch, int min, int max) {
+            this.isSearch = isSearch;
+            this.min = min;
+            this.max = max;
+        }
+    }
+
+
+    public static boolean isBST2(Node head) {
+        if (head == null) {
+            return true;
+        }
+        return process(head).isSearch;
+    }
+
+    public static Info process(Node head) {
+        if (head == null) {
+            return null;
+        }
+        Info leftInfo = process(head.left);
+        Info rightInfo = process(head.right);
+        int min = head.value;
+        int max = head.value;
+        boolean isSearch = true;
+        if (leftInfo != null) {
+            min = Math.min(min, leftInfo.min);
+            max = Math.max(max, leftInfo.max);
+        }
+        if (rightInfo != null) {
+            min = Math.min(min, rightInfo.min);
+            max = Math.max(max, rightInfo.max);
+        }
+        if (leftInfo != null && !leftInfo.isSearch) {
+            isSearch = false;
+        }
+
+        if (rightInfo != null && !rightInfo.isSearch) {
+            isSearch = false;
+        }
+
+        if (leftInfo != null && leftInfo.max >= head.value) {
+            isSearch = false;
+        }
+
+        if (rightInfo != null && rightInfo.min <= head.value) {
+            isSearch = false;
+        }
+
+        return new Info(isSearch, min, max);
+
+    }
+
+    // for test
+    public static Node generateRandomBST(int maxLevel, int maxValue) {
+        return generate(1, maxLevel, maxValue);
+    }
+
+    // for test
+    public static Node generate(int level, int maxLevel, int maxValue) {
+        if (level > maxLevel || Math.random() < 0.5) {
+            return null;
+        }
+        Node head = new Node((int) (Math.random() * maxValue));
+        head.left = generate(level + 1, maxLevel, maxValue);
+        head.right = generate(level + 1, maxLevel, maxValue);
+        return head;
+    }
+
+    public static void main(String[] args) {
+        int maxLevel = 4;
+        int maxValue = 100;
+        int testTimes = 1000000;
+        for (int i = 0; i < testTimes; i++) {
+            Node head = generateRandomBST(maxLevel, maxValue);
+            if (isBST1(head) != isBST2(head)) {
+                System.out.println("Oops!");
+            }
+        }
+        System.out.println("finish!");
+    }
+
+
+}
---
+++ src/io/arkvaer/algorithm/basic/day12/IsCBT.java
@@ -0,0 +1,151 @@
+package io.arkvaer.algorithm.basic.day12;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * 判断二叉树是否为完全二叉树
+ *
+ * @author waver
+ * @date 2023/7/15 15:52
+ */
+public class IsCBT {
+
+    public static class Node {
+        public int value;
+        public Node left;
+        public Node right;
+
+        public Node(int data) {
+            this.value = data;
+        }
+    }
+
+    /**
+     * 使用按层遍历的方式判断是否为完全二叉树
+     *
+     * @return 是否为完全二叉树
+     */
+    public static boolean isCbt1(Node head) {
+        // 空树时认为是完全二叉树
+        if (head == null) {
+            return true;
+        }
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(head);
+        // 是否已经遇到左右孩子不全的节点?
+        boolean isLeaf = false;
+        Node cur;
+        while (!queue.isEmpty()) {
+            cur = queue.poll();
+            // 当遇到过左右孩子不双全的节点后再出现左右孩子双全节点则不是完全二叉树
+            if ((isLeaf && (cur.left != null || cur.right != null)) ||
+                    // 当某个节点的左孩子为空, 右孩子不为空时, 则不是完全二叉树
+                    (cur.left == null && cur.right != null)) {
+
+                return false;
+            }
+            if (cur.left != null) {
+                queue.add(cur.left);
+            }
+            if (cur.right != null) {
+                queue.add(cur.right);
+            }
+            if (cur.left == null || cur.right == null) {
+                isLeaf = true;
+            }
+        }
+        return true;
+    }
+
+    public static class Info {
+        /**
+         * 是否为满二叉树
+         */
+        boolean isFull;
+        /**
+         * 是否为完全二叉树
+         */
+        boolean isComplete;
+        /**
+         * 树的高度
+         */
+        int height;
+
+        public Info(boolean isFull, boolean isComplete, int height) {
+            this.isFull = isFull;
+            this.isComplete = isComplete;
+            this.height = height;
+        }
+    }
+
+    public static boolean isCbt2(Node head) {
+        if (head == null) {
+            return true;
+        }
+        return process(head).isComplete;
+    }
+
+    public static Info process(Node head) {
+        if (head == null) {
+            return new Info(true, true, 0);
+        }
+        Info leftInfo = process(head.left);
+        Info rightInfo = process(head.right);
+        boolean isFull = false;
+        boolean isComplete = false;
+        int height = Math.max(leftInfo.height, rightInfo.height) + 1;
+        // 左子树和右子树都是满二叉树, 且左右子树高度相等, 则当前树也是满二叉树, 当然也是完全二叉树
+        if (leftInfo.isFull && rightInfo.isFull && leftInfo.height == rightInfo.height) {
+            isFull = true;
+            isComplete = true;
+        } else {
+            // 若左子树和右子树都是满二叉树, 且左子树的高度 == 右子树的高度 + 1, 则该二叉树是完全二叉树
+            if (leftInfo.isFull && rightInfo.isFull && leftInfo.height == rightInfo.height + 1) {
+                isComplete = true;
+            }
+
+            // 若左子树是满二叉树, 右子树是完全二叉树(不是满二叉树) 且左子树高度==右子树高度, 则该树为完全二叉树
+            if (leftInfo.isFull && rightInfo.isComplete && leftInfo.height == rightInfo.height) {
+                isComplete = true;
+            }
+
+            // 若左子树为完全二叉树(不是满二叉树), 且右子树是满二叉树时, 左子树高度 == 右子树高度 + 1, 则该树为完全二叉树
+            if (leftInfo.isComplete && rightInfo.isFull && leftInfo.height == rightInfo.height + 1) {
+                isComplete = true;
+            }
+        }
+
+        return new Info(isFull, isComplete, height);
+    }
+
+    // for test
+    public static Node generateRandomBST(int maxLevel, int maxValue) {
+        return generate(1, maxLevel, maxValue);
+    }
+
+    // for test
+    public static Node generate(int level, int maxLevel, int maxValue) {
+        if (level > maxLevel || Math.random() < 0.5) {
+            return null;
+        }
+        Node head = new Node((int) (Math.random() * maxValue));
+        head.left = generate(level + 1, maxLevel, maxValue);
+        head.right = generate(level + 1, maxLevel, maxValue);
+        return head;
+    }
+
+    public static void main(String[] args) {
+        int maxLevel = 5;
+        int maxValue = 100;
+        int testTimes = 1000000;
+        for (int i = 0; i < testTimes; i++) {
+            Node head = generateRandomBST(maxLevel, maxValue);
+            if (isCbt1(head) != isCbt2(head)) {
+                System.out.println("Oops!");
+            }
+        }
+        System.out.println("finish!");
+    }
+
+}
