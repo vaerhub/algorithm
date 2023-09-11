b08432b191bf79f5929743b6140a1a7f39b2103b
arkvaer <arkvaer@gmail.com>
2023-09-11 15:28:15 +0800

    add: KMP test

--- src/io/arkvaer/algorithm/basic/day27/KMP.java
+++ src/io/arkvaer/algorithm/basic/day27/KMP.java
@@ -1,59 +1,75 @@
 package io.arkvaer.algorithm.basic.day27;
 
-import io.arkvaer.algorithm.utils.AlgUtil;
-
-/**
- * @author waver
- * @date 2023/9/10 21:23
- */
 public class KMP {
-    public static int getIndexOf(String src, String target) {
-        if (src == null || target == null || src.isEmpty() || target.isEmpty()) {
+
+    public static int getIndexOf(String src, String match) {
+        if (src == null || match == null || src.isEmpty() || match.isEmpty()) {
             return -1;
         }
-        char[] c1 = src.toCharArray();
-        char[] c2 = target.toCharArray();
+        char[] s = src.toCharArray();
+        char[] m = match.toCharArray();
+        int[] next = getNextArray(m);
         int x = 0;
         int y = 0;
-        int[] next = getNextArray(c2);
-        while (x < c1.length && y < c2.length) {
-            if (c1[x] == c2[y]) {
+        while (x < s.length && y < m.length) {
+            if (s[x] == m[y]) {
+                // s当前位置 与 m 当前位置 相等, 则同时++ 继续匹配下个字符
                 x++;
                 y++;
-            } else if (next[y] == -1) {
+            } else if (next[y] == -1) {  // 即 y == 0, 只有在 y==0 时 next[y] 是 -1
+                // 如果 m 的第一个字符与 s 当前位置不相等, m 再无法右移 则使用 s 的下一个字符 与 m 的第一个字符比较
                 x++;
             } else {
+                // s当前位置 与 m 当前位置 不相等, m 跳到当前位置的 next 位置
                 y = next[y];
             }
         }
-        return y == c2.length ? x - y : -1;
+        return y == m.length ? x - y : -1;
     }
 
-    private static int[] getNextArray(char[] c) {
-        if (c.length == 1) {
+    /**
+     * 当前位置 前缀串 与 后缀串 最长的相等 的[长度]
+     *
+     * @param match 给定字符串 char数组
+     * @return 当前位置 前缀串 与 后缀串 最长的相等 的[长度] 的数组
+     */
+    public static int[] getNextArray(char[] match) {
+        if (match.length == 1) {
             return new int[]{-1};
         }
-        int[] next = new int[c.length];
+        int[] next = new int[match.length];
         next[0] = -1;
         next[1] = 0;
-        int i = 2;
-        int cur = 0;
-        while (i < next.length) {
-            if (c[i - 1] == c[cur]) {
-                next[i++] = ++cur;
-            } else if (cur > 0) {
-                cur = next[cur];
+        // 前缀的坐标
+        int prefix = 0;
+        // 当前坐标
+        int cur = 2;
+        /*
+        prefix
+          ↓
+        abcdabce
+               ↑
+              cur
+         */
+        while (cur < match.length) {
+            if (match[cur - 1] == match[prefix]) {
+                // 当前位置 前缀字符和后缀字符匹配成功, 当前位置的next数组设置为前缀/后缀串的长度,
+                next[cur++] = ++prefix;
+            } else if (prefix > 0) {
+                // 若不匹配, 将前缀的位置移动到
+                prefix = next[prefix];
             } else {
-                next[i++] = 0;
+                next[cur++] = 0;
             }
         }
         return next;
     }
 
+    // region test
     public static String getRandomString(int possibilities, int size) {
-        char[] ans = new char[AlgUtil.random.nextInt(size) + 1];
+        char[] ans = new char[(int) (Math.random() * size) + 1];
         for (int i = 0; i < ans.length; i++) {
-            ans[i] = (char) (AlgUtil.random.nextInt(possibilities) + 'a');
+            ans[i] = (char) ((int) (Math.random() * possibilities) + 'a');
         }
         return String.valueOf(ans);
     }
@@ -73,4 +89,5 @@ public class KMP {
         }
         System.out.println("test finish");
     }
+    // endregion
 }
