0300601b87d5ed044af0af99ec16914ad22f77c9
arkvaer <arkvaer@gmail.com>
2023-09-15 20:11:32 +0800

    add: Manacher.java

--- src/io/arkvaer/algorithm/basic/day28/Manacher.java
+++ src/io/arkvaer/algorithm/basic/day28/Manacher.java
@@ -20,7 +20,7 @@ public class Manacher {
         int max = Integer.MIN_VALUE;
         for (int i = 0; i < chars.length; i++) {
             // 当 R > i 时，以i为中心点的回文字串的最小长度为 i 关于中心的对称点的回文长度和
-            radiusArr[i] = R > i ? Math.min(radiusArr[2 * C - 1], R - i) : 1;
+            radiusArr[i] = R > i ? Math.min(radiusArr[2 * C - i], R - i) : 1;
             while (i + radiusArr[i] < chars.length && i - radiusArr[i] > -1) {
                 if (chars[i + radiusArr[i]] == chars[i - radiusArr[i]]) {
                     radiusArr[i]++;
@@ -51,5 +51,48 @@ public class Manacher {
             result[i] = (i & 1) == 0 ? '#' : chars[index++];
         }
         return result;
+    }
+
+
+    // for test
+    public static int right(String s) {
+        if (s == null || s.length() == 0) {
+            return 0;
+        }
+        char[] str = preprocessing(s);
+        int max = 0;
+        for (int i = 0; i < str.length; i++) {
+            int L = i - 1;
+            int R = i + 1;
+            while (L >= 0 && R < str.length && str[L] == str[R]) {
+                L--;
+                R++;
+            }
+            max = Math.max(max, R - L - 1);
+        }
+        return max / 2;
+    }
+
+    // for test
+    public static String getRandomString(int possibilities, int size) {
+        char[] ans = new char[(int) (Math.random() * size) + 1];
+        for (int i = 0; i < ans.length; i++) {
+            ans[i] = (char) ((int) (Math.random() * possibilities) + 'a');
+        }
+        return String.valueOf(ans);
+    }
+
+    public static void main(String[] args) {
+        int possibilities = 5;
+        int strSize = 20;
+        int testTimes = 5000000;
+        System.out.println("test begin");
+        for (int i = 0; i < testTimes; i++) {
+            String str = getRandomString(possibilities, strSize);
+            if (manacher(str) != right(str)) {
+                System.out.println("Oops!");
+            }
+        }
+        System.out.println("test finish");
     }
 }
