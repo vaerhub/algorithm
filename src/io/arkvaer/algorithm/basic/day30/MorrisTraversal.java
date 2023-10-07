6f6cb472b91a32ecb60d444709878cc8c312a347
arkvaer <arkvaer@gmail.com>
2023-10-07 15:14:14 +0800

    feat: MorrisTraversal 后序遍历

--- src/io/arkvaer/algorithm/basic/day30/MorrisTraversal.java
+++ src/io/arkvaer/algorithm/basic/day30/MorrisTraversal.java
@@ -55,7 +55,7 @@ public class MorrisTraversal {
         while (cur != null) {
             mostRight = cur.left;
             if (Objects.nonNull(mostRight)) {
-                while (Objects.nonNull(mostRight.right) && !Objects.equals(cur, mostRight)) {
+                while (Objects.nonNull(mostRight.right) && !Objects.equals(cur, mostRight.right)) {
                     mostRight = mostRight.right;
                 }
                 if (Objects.isNull(mostRight.right)) {
@@ -66,7 +66,6 @@ public class MorrisTraversal {
                     mostRight.right = null;
                 }
             }
-
             // 情况 1 和情况 2 中的 (b 会共用
             cur = cur.right;
         }
@@ -91,7 +90,7 @@ public class MorrisTraversal {
                     mostRight = mostRight.right;
                 }
                 if (Objects.isNull(mostRight.right)) {
-                    System.out.println(cur.value + " ");
+                    System.out.print(cur.value + " ");
                     mostRight.right = cur;
                     cur = cur.left;
                     continue;
@@ -99,10 +98,11 @@ public class MorrisTraversal {
                     mostRight.right = null;
                 }
             } else {
-                System.out.println(cur.value + " ");
+                System.out.print(cur.value + " ");
             }
             cur = cur.right;
         }
+        System.out.println();
     }
 
     /**
@@ -131,9 +131,59 @@ public class MorrisTraversal {
                     mostRight.right = null;
                 }
             }
-            System.out.println(cur.value + " ");
+            System.out.print(cur.value + " ");
             cur = cur.right;
         }
+        System.out.println();
+    }
+
+    public static void morrisPos(Node head) {
+        if (head == null) {
+            return;
+        }
+        Node cur = head;
+        Node mostRight;
+        while (cur != null) {
+            mostRight = cur.left;
+            if (mostRight != null) {
+                while (mostRight.right != null && mostRight.right != cur) {
+                    mostRight = mostRight.right;
+                }
+                if (mostRight.right == null) {
+                    mostRight.right = cur;
+                    cur = cur.left;
+                    continue;
+                } else {
+                    mostRight.right = null;
+                    printEdge(cur.left);
+                }
+            }
+            cur = cur.right;
+        }
+        printEdge(head);
+        System.out.println();
+    }
+
+    public static void printEdge(Node head) {
+        Node tail = reverseEdge(head);
+        Node cur = tail;
+        while (cur != null) {
+            System.out.print(cur.value + " ");
+            cur = cur.right;
+        }
+        reverseEdge(tail);
+    }
+
+    public static Node reverseEdge(Node from) {
+        Node pre = null;
+        Node next;
+        while (from != null) {
+            next = from.right;
+            from.right = pre;
+            pre = from;
+            from = next;
+        }
+        return pre;
     }
 
     public static void main(String[] args) {
@@ -144,9 +194,10 @@ public class MorrisTraversal {
         head.left.right = new Node(5);
         head.right.left = new Node(6);
         head.right.right = new Node(7);
-        // morris(head);
-        // morrisPre(head);
+        morris(head);
+        morrisPre(head);
         morrisIn(head);
+        morrisPos(head);
     }
 
 
