50a95335f42266402206e40adb7164de2812f30a
arkvaer <arkvaer@gmail.com>
2023-10-15 22:45:59 +0800

    feat: ACAutomation

--- src/io/arkvaer/algorithm/basic/day32/AC1.java
+++ src/io/arkvaer/algorithm/basic/day32/AC1.java
@@ -1,11 +1,17 @@
 package io.arkvaer.algorithm.basic.day32;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  * AC 自动机
  */
 public class AC1 {
 
     public static class Node {
+        /**
+         * 有多少个字符串以该节点结尾
+         */
         private int end;
         private Node fail;
         private Node[] nextArr;
@@ -35,6 +41,38 @@ public class AC1 {
                 cur = cur.nextArr[index];
             }
             cur.end++;
+        }
+
+        public void build() {
+            Queue<Node> queue = new LinkedList<>();
+            queue.add(root);
+            // 当前节点
+            Node father;
+            Node faFail;
+            while (!queue.isEmpty()) {
+                father = queue.poll();
+                // 检查当前的节点所有自节点
+                for (int i = 0; i < 26; i++) {
+                    // 当前子节点不为空
+                    if (father.nextArr[i] != null) {
+                        // 初始预设fail节点为root节点
+                        father.nextArr[i].fail = root;
+                        // 父节点的fail节点
+                        faFail = father.fail;
+                        // 如果父节点的fail不为空
+                        while (faFail != null) {
+                            // 父节点的fail节点是否有指向 与当前子节点相同的子节点
+                            if (faFail.nextArr[i].fail != null) {
+                                // 如果存在, 则将当前节点的fail指向该子节点
+                                father.nextArr[i].fail = faFail.nextArr[i];
+                                break;
+                            }
+                            // 若不存在, 则找到父节点的fail节点
+                            faFail = faFail.fail;
+                        }
+                    }
+                }
+            }
         }
 
 
